857afb74be9d25b93112df5436caf054ff3cbc7d
Peter Biro <peter.biro@oliasoft.com>
2023-10-18 10:19:16 +0200

    make the light operating

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const STOP_TIME = 3000;
+const SLOW_TIME = 2000;
+const GO_TIME = 5000;
 
 type LightState = 'stop' | 'slow' | 'go'
 
 function App() {
-  const [state, lightState] = useState<LightState>('slow');
+  const [state, lightState] = useState<LightState>('stop');
 
   const getLightClassName = (light: LightState) => {
     return `light ${light} ` + (state === light ? "on" : "");
   }
+
+  useEffect(() => {
+    if (state === 'stop') {
+      setInterval(() => {lightState('go')}, STOP_TIME)
+      return;
+    } else 
+    if (state === 'slow') {
+      setInterval(() => {lightState('stop')}, SLOW_TIME)
+      return;
+    }
+    if (state === 'go') {
+      setInterval(() => {lightState('slow')}, GO_TIME)
+      return;
+    }
+
+  }, [state])
 
   return (
     <div className='stop-light'>
